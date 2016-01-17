6c580da85ba3aff631b3981d8c53304177eabe1a
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-16 18:51:16 -1000

    installed pg and pg-hstore

--- models/users.js
+++ models/users.js
@@ -3,7 +3,13 @@ module.exports = function(sequelize, DataTypes) {
   var Users = sequelize.define('Users', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    bio: DataTypes.TEXT
+    bio: DataTypes.TEXT,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true
+      }
+    }
   }, {
     classMethods: {
       associate: function(models) {
