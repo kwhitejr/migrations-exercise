1937edf5418ae3107435012a7e9e35a003f19781
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-16 18:23:28 -1000

    sequelize created model and mgration

---
+++ models/users.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Users = sequelize.define('Users', {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    bio: DataTypes.TEXT
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Users;
+};
